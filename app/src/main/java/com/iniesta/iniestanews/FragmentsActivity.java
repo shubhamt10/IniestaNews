7a430e4eb7c06cd2b7468ed56ca312e80ea7970e
Shubham Tripathi <tripathi.shubham509@gmail.com>
2019-06-11 13:29:12 +0530

    changes to fragment activity

--- app/src/main/java/com/iniesta/iniestanews/FragmentsActivity.java
+++ app/src/main/java/com/iniesta/iniestanews/FragmentsActivity.java
@@ -14,7 +14,6 @@ public class FragmentsActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.fragments_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         String type = getIntent().getStringExtra("type");
         Bundle bundle1 = new Bundle();
@@ -22,56 +21,67 @@ public class FragmentsActivity extends AppCompatActivity {
         Fragment fragment = new HomeFragment();
         switch (type){
             case "latest":
+                getSupportActionBar().setTitle("Latest News");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "entertainment":
+                getSupportActionBar().setTitle("Entertainment");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=entertainment&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "politics":
+                getSupportActionBar().setTitle("Politics");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=general&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "gadgets":
+                getSupportActionBar().setTitle("Gadgets");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=technology&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "delhiNCR":
+                getSupportActionBar().setTitle("Delhi-NCR");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=general&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "sports":
+                getSupportActionBar().setTitle("Sports");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=sports&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "business":
+                getSupportActionBar().setTitle("Business");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=business&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "health":
+                getSupportActionBar().setTitle("Health");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=health&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "education":
+                getSupportActionBar().setTitle("Education");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=general&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "world":
-                bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=general&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
+                getSupportActionBar().setTitle("World");
+                bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=general&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
                 break;
             case "national":
+                getSupportActionBar().setTitle("National");
                 bundle1.putString("url", "https://newsapi.org/v2/top-headlines?category=general&country=in&pageSize=20&apiKey=598ae4e3c5c940ff991d7f44b9f3dde6");
                 fragment = new HomeFragment();
                 fragment.setArguments(bundle1);
