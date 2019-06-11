3e83181b74b7ff75742e1fa68f3197ab7e24739c
Ranger12021999 <amansharma123a@gmail.com>
2019-06-11 13:13:38 +0530

    newsDetailActivity completed 1

--- app/src/main/java/com/iniesta/iniestanews/settings.java
+++ app/src/main/java/com/iniesta/iniestanews/settings.java
@@ -24,12 +24,14 @@ import com.bumptech.glide.util.Util;
  */
 public class settings extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     Button b1;
 
     // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
