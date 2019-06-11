4df898c3b0dd12244261233fb2a80a3a3275a57a
Shubham Tripathi <tripathi.shubham509@gmail.com>
2019-06-11 14:58:34 +0530

    changes to fragment activity

--- app/src/main/java/com/iniesta/iniestanews/feedback.java
+++ app/src/main/java/com/iniesta/iniestanews/feedback.java
@@ -1,7 +1,5 @@
 package com.iniesta.iniestanews;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,13 +9,6 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * factory method to
- * create an instance of this fragment.
- */
 public class feedback extends Fragment {
     ImageButton submit;
     EditText name,email,text;
@@ -50,21 +41,6 @@ public class feedback extends Fragment {
     }
 }
 
-
-
-
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
 
 
 
--- app/src/main/java/com/iniesta/iniestanews/settings.java
+++ app/src/main/java/com/iniesta/iniestanews/settings.java
@@ -19,34 +19,13 @@ import com.bumptech.glide.util.Util;
 
 import static android.content.Context.MODE_PRIVATE;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * interface
- * to handle interaction events.
- * Ufactory method to
- * create an instance of this fragment.
- */
 public class settings extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-  Switch b1;
-
-    // TODO: Rename and change types of parameters
-
-
-
-
-
-
+    Switch b1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
 
@@ -54,10 +33,9 @@ public class settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        View v=inflater.inflate(R.layout.fragment_settings, container, false);
-       b1=v.findViewById(R.id.button);
-       b1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+        b1 = v.findViewById(R.id.button);
+        b1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
            @Override
            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                if(isChecked)
@@ -65,16 +43,12 @@ public class settings extends Fragment {
                    Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
 
                }
-           }
+            }
        });
 
 
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-
-
-
-        }
+}
 
